fe4cc1070a313b01e78bebbb30fe92817c1fe7bc
taiwo2 <adetonat@ymail.com>
2024-05-09 11:24:34 +0100

    Add company page

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -7,6 +7,8 @@ import {Company,JobAboutJobFooter,JobTabs,ScreenHeaderBtn,Specifics,
 import { COLORS, icons, SIZES } from "../../constants";
 import useFetch from "../../hook/useFetch";
 
+const tabs = ["About", "Qualifications", "Responsibilities"];
+
 const JobDetails = () => {
   const params = useLocalSearchParams();
   const router = useRouter();
@@ -16,6 +18,12 @@ const JobDetails = () => {
   const { data, isLoading, error, refetch } = useFetch("job-details", {
     job_id: params.id,
   });
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    refetch()
+    setRefreshing(false)
+  }, []);
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -49,28 +57,28 @@ const JobDetails = () => {
             <Text>Something went wrong</Text>
           ) : data.length === 0 ? (
             <Text>No data available</Text>
-          ) : ( 'tull'
-            // <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
-            //   <Company
-            //     companyLogo={data[0].employer_logo}
-            //     jobTitle={data[0].job_title}
-            //     companyName={data[0].employer_name}
-            //     location={data[0].job_country}
-            //   />
+          ) : ( 
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
 
-            //   <JobTabs
-            //     tabs={tabs}
-            //     activeTab={activeTab}
-            //     setActiveTab={setActiveTab}
-            //   />
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
 
-            //   {displayTabContent()}
-            // </View>
+              {/* {displayTabContent()} */}
+            </View>
           )
           }
         </ScrollView>
 
-        <JobFooter url={data[0]?.job_google_link ?? 'https://careers.google.com/jobs/results/'} />
+        {/* <JobFooter url={data[0]?.job_google_link ?? 'https://careers.google.com/jobs/results/'} /> */}
       </>
     </SafeAreaView>
   );
